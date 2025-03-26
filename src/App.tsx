42ee03de39645677b20efaaf9cda4e658b84e4b6
Itxhamza7 <hmzaakram295@gmail.com>
2025-03-26 23:31:26 +0500

    wo

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { Header } from './components/Header';
 import { Home } from './pages/Home';
-import { Profile } from './pages/Profile.tsx';
+import { Profile } from './pages/Profile';
 import { Messages } from './pages/Messages.tsx';
 import { EditProfile } from './pages/EditProfile.tsx';
 import { Reels } from './pages/Reels.tsx';
--- src/pages/Messages.tsx
+++ src/pages/Messages.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+// import React from 'react';
 import { useParams } from 'react-router-dom';
 import { Search, Phone, Video, Info } from 'lucide-react';
 
